e3d0e3b859defbb537287e14dd5336586a4233dc
Kemuro <antonkemuro@gmail.com>
2021-04-09 12:23:43 -0500

    created overloading exercise

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(12.45, 45.012));
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool isValid)
+        {
+            var sum = x + y;
+            if (isValid == true)
+            {
+                if (sum == 1)
+                    return "1 dollar";
+                else return $"{sum} dollars";
+            }
+            else
+                return $"{sum}";
+
+            //use below code if parameter was asking for strings
+            //var num1 = int.TryParse(x, out int numX);
+            //var num2 = int.TryParse(y, out int numY);
+            
+        }
+    }
+}
